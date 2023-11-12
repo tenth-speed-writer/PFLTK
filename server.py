04a443d541e8967412a9d359ca7c47f0e9a4fdd7
Louise Seale <addseale2@gmail.com>
2023-11-12 17:49:04 -0600

    Creates a hello-world-ish basic server script/module

--- server.py
+++ server.py
@@ -2,12 +2,30 @@
 various features to their respective endpoints."""
 
 import discord
+import config
+
+# Get the bot's token from the config file
+token = config.discord_app_token
 
 # Configure an Intents instance for the message_content intent
 intents = discord.Intents.default()
 intents.message_content = True
 
-# Initialie the client using the specified intents
+# Initialize the client using the specified intents
 client = discord.Client(intents=intents)
 
+# Set an event to announce in log when ready
+@client.event
+async def on_ready():
+    print(f'PFL-TK logged in as {client.user}')
 
+# Set an event to handle incoming messages
+@client.event
+async def on_message(message: discord.Message):
+    response = \
+        f'Greetings {message.author}. Your message reads: {message.content}'
+    
+    await message.channel.send(response)
+
+if __name__ == "__main__":
+    client.run(token=token)
