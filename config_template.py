d211fc3460341441bc1324ab335048b3ab04db6f
Louise Seale <addseale2@gmail.com>
2023-11-15 12:20:04 -0600

    Add a DB connection string to the configs

--- config_template.py
+++ config_template.py
@@ -1,3 +1,4 @@
 # Fill in your bot credentials and copy this file to "config.py"
 discord_app_token = ""
 discord_bot_account_name = ""
+db_connection_string = "pfltk.db" # A local SQLite3 file by default
