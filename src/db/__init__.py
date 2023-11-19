79f48607983ea45c37e5520077b65ff3ec59ec16
Louise Seale <addseale2@gmail.com>
2023-11-19 14:34:46 -0600

    Adds 2 distinctive exceptions to the DB module

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,3 +1,5 @@
 from db import DB
-
+from exceptions import \
+    MultipleUniqueRowsException, \
+    UniqueRowNotFoundException
 __all__ = ["DB"]
---
+++ src/db/exceptions.py
@@ -0,0 +1,18 @@
+"""Contains exceptions shared between the database interaction methods"""
+
+class MultipleUniqueRowsException(Exception):
+    """
+    Represents an exception where a query which should have returned a
+    single unique row instead returned multiple rows.
+    """
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+    
+class UniqueRowNotFoundException(Exception):
+    """
+    Represents an exception where a query which should have returned a
+    single row's worth of data based on its primary key or other unique
+    identifier instead returned no rows.
+    """
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
