0c7fa8885fd280da3959ec1f1afeab2bfe176047
Louise Seale <addseale2+github@gmail.com>
2023-11-23 13:53:26 -0600

    Adds a test for the get_war() function of the war API module

--- test/test_warAPI.py
+++ test/test_warAPI.py
@@ -3,4 +3,9 @@ import pytest
 from ..src import warAPI as api
 
 class TestGetWar:
-    pass
+    def test_gets_war_number_correctly(self):
+        """Assures it can correctly get the war number from the API"""
+        war_num, pulled_on = api.get_war()
+
+        assert 0 < war_num
+        assert war_num < 1000
