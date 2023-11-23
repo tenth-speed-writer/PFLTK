b972f4083597c84f05b5cea22996f7a8647928d0
Louise Seale <addseale2+github@gmail.com>
2023-11-22 21:59:28 -0600

    Adds the second-half URL pieces for the API

--- config_template.py
+++ config_template.py
@@ -10,20 +10,30 @@ discord_bot_account_name = ""
 # SQLite3 connection string for data storage
 db_connection_string = "pfltk.db" # A local SQLite3 file by default
 
-# Server-specific API addresses roots
-war_api_endpoints = SimpleNamespace()
+# # # Server-specific API addresses roots
+war_api_roots = SimpleNamespace()
 
 # Able URL
-war_api_endpoints.live_1 = 'https://war-service-live.foxholeservices.com/api/'
+war_api_roots.live_1 = 'https://war-service-live.foxholeservices.com/api/'
 
 # Bravo URL
-war_api_endpoints.live_2 = 'https://war-service-live-2.foxholeservices.com/api/'
+war_api_roots.live_2 = 'https://war-service-live-2.foxholeservices.com/api/'
 
 # Charlie URL
-war_api_endpoints.live_3 = 'https://war-service-live-3.foxholeservices.com/api/'
+war_api_roots.live_3 = 'https://war-service-live-3.foxholeservices.com/api/'
 
 # Dev server URL
-war_api_endpoints.dev = 'https://war-service-dev.foxholeservices.com/api/'
+war_api_roots.dev = 'https://war-service-dev.foxholeservices.com/api/'
 
 # Which of these links the app should use
-war_api_endpoints.selected = war_api_endpoints.live_1
+war_api_roots.selected = war_api_roots.live_1
+
+# # # API Endpoint Suffixes
+war_api_endpoints = SimpleNamespace()
+
+war_api_endpoints.war = "war/"
+war_api_endpoints.maps = "maps/"
+
+# Static and Dynamic data endpoints should have their map name formatted in
+war_api_endpoints.static_map_data = 'maps/{map_name}/static/'
+war_api_endpoints.dynamic_map_data = 'maps/{map_name}/dynamic/public'
