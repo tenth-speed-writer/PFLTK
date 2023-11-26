717bf9e0761b1c373027d9f08784a25d64392b9f
Louise Seale <addseale2+github@gmail.com>
2023-11-26 15:40:28 -0600

    Adds get_labels(), docstring for get_maps()

--- src/warAPI/__init__.py
+++ src/warAPI/__init__.py
@@ -36,6 +36,13 @@ def get_war() -> Tuple[int, datetime.datetime]:
     return war_num, pulled_on
 
 def get_maps() -> List[str]:
+    """
+    Makes a GET request to the /maps endpoint of the War API, returning
+    a list of the unique names of hexes present in this war's map.
+
+    Returns:
+        List[str]: A list of unique hex map names in the current war
+    """
     # Assemble the URL for the /maps endpoint
     root = config.war_api_roots.selected
     suffix = config.war_api_endpoints.maps
@@ -46,3 +53,50 @@ def get_maps() -> List[str]:
 
     # Deserialize and return the content
     return response.json()
+
+def get_labels(
+        map_name: str,
+        label_type="Major") -> List[Tuple[str, int, int, str]]:
+    """
+    Makes a GET request to the /maps/:mapName/static endpoint, returning the
+    major and minor text labels which identify the geography of that map as
+    well as the hex-relative X and Y positions and "Major" or "Minor" type
+    values for each of those labels.
+    Args:
+        map_name (str): The hex for which to pull major and minor labels.
+        label_type (str, optional): "Major", "Minor", or "Both". Defaults to "Both".
+
+    Returns:
+        List[Tuple[str, int, int, str]]: A tuple containing
+        (label text, x, y, and label type)
+    """
+    # Assemble URL, including map name parameter
+    root = config.war_api_roots.selected
+    suffix = config.war_api_endpoints.static_map_data.format(map_name=map_name)
+    url = root + suffix
+
+    # Make the request and parse the results to JSON.
+    # Result format is: [("text", "x", "y", "mapMarkerType"), ...]
+    results = requests.get(url).json()
+
+    # Filter--if desired--by label type and return the list of tuples
+    if label_type.lower() == "major":
+        return [
+            (item["text"], item["x"], item["y"], item["mapMarkerType"])
+            for item in results["mapTextItems"]
+            if item["mapMarkerType"] == "Major"
+        ]
+    elif label_type.lower() == "minor":
+        return [
+            (item["text"], item["x"], item["y"], item["mapMarkerType"])
+            for item in results["mapTextItems"]
+            if item["mapMarkerType"] == "Minor"
+        ]
+    elif label_type.lower() == "both":
+        return [
+            (item["text"], item["x"], item["y"], item["mapMarkerType"])
+            for item in results["mapTextItems"]
+        ]
+    else:
+        raise ValueError("label_type must be one of \"Major\", \"Minor\", or \"Both\". Got {val}".format(val=label_type))
+    
--- test/test_warAPI.py
+++ test/test_warAPI.py
@@ -3,6 +3,7 @@ import pytest
 from ..src import warAPI as api
 
 class TestGetWar:
+    """Contains methods for testing the warAPI.get_war() method"""
     def test_gets_war_number_correctly(self):
         """Assures it can correctly get the war number from the API"""
         war_num, pulled_on = api.get_war()
@@ -11,6 +12,7 @@ class TestGetWar:
         assert war_num < 1000
 
 class TestGetMaps:
+    """Contains methods for testing the warAPI.get_maps() method"""
     def tests_gets_maps_correctly(self):
         """Assures it can correctly get the list of map names"""
 
@@ -20,3 +22,68 @@ class TestGetMaps:
 
         # Check for fingers~
         assert map_to_check in results
+
+class TestGetLabels:
+    """Contains methods for testing the warAPI.get_labels() method"""
+    # Constants to test with
+    map_hex = "TheFingersHex"
+    known_fingers_major_label = "Captain's Dread"
+    known_fingers_minor_label = "Cavitatis"
+
+    def test_gets_all_labels_correctly(self):
+        """Assures it fetches lists of major labels"""
+        # Get its labels
+        result = api.get_labels(
+            map_name=self.map_hex,
+            label_type="Both"
+        )
+
+        # Assert the list of "text" attributes of the contents of
+        # result["mapTextItems"] contains our desired major & minor labels
+        texts = [text for text, x, y, label_type in result]
+
+        assert self.known_fingers_major_label in texts
+        assert self.known_fingers_minor_label in texts
+    
+    def test_gets_major_labels_correctly(self):
+        """Assures it fetches a list of only major labels"""
+        # Get its labels
+        result = api.get_labels(
+            map_name=self.map_hex,
+            label_type="Major"
+        )
+
+        # Assert the list of "text" attributes of the contents of
+        # result["mapTextItems" contains our desired major label
+        # and NOT our known minor label.
+
+        texts = [text for text, x, y, label_type in result]
+
+        assert self.known_fingers_major_label in texts
+        assert not (self.known_fingers_minor_label in texts)
+
+    def test_gets_minor_labels_correctly(self):
+        """Assures it fetches a list of only minor labels"""
+        # Assert the list of "text" attributes of the contents of
+        # result["mapTextItems"] contains our desired minor label
+        # and NOT our known major label.
+        result = api.get_labels(
+            map_name=self.map_hex,
+            label_type="Minor"
+        )
+
+        texts = [text for text, x, y, label_type in result]
+
+        assert not (self.known_fingers_major_label in texts)
+        assert self.known_fingers_minor_label in texts
+
+    def test_raises_value_error_for_bad_label_type(self):
+        """
+        Assures a label_type besides 'Major', 'Minor', or 'Both'
+        raises a ValueError from the function.
+        """
+
+        with pytest.raises(ValueError):
+            api.get_labels(self.map_hex, "doobie")
+    
+    
