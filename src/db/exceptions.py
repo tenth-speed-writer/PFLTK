3c6522abd55504d30c7987872ae202db905da042
Louise Seale <addseale2+github@gmail.com>
2023-12-07 11:27:48 -0600

    Adds an exception for cases where a call to db.icons or db.labels refers to a map for which there is no row in the current war

--- src/db/exceptions.py
+++ src/db/exceptions.py
@@ -34,3 +34,36 @@ class NoDataReturnedException(Exception):
     """
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
+
+class MapDoesNotExistInCurrentWarException(Exception):
+    """
+    Represents an exception where-in a call for something uniquely identified
+    for a certain map in a certain war (e.g. icons, labels) is requested but
+    there does not exist a row that map for the war in question.
+
+    Indicates that a call was made referring to maps in the current war before
+    or in absence of the maps stage of initialization being completed.
+    """
+    # def __init__(self, *args: object) -> None:
+    #     super().__init__(*args)
+
+    def __init__(
+        map_name: str,
+        last_known_war: int|None = None
+    ) -> None:
+        # Change message slightly depending on whether
+        # the last_known_war was provided or not
+        if last_known_war is not None:
+            message = \
+                f"A request was made referring to map {map_name}; " \
+                f"however, no row exists in the 'maps' table for " \
+                f"{map_name} in the current war. The last known war "\
+                f"in which this map occurred was {str(last_known_war)}."
+        else:
+            message = \
+                f"A request was made referring to map {map_name}; " \
+                f"however, no row exists in the 'maps' table for " \
+                f"{map_name} in the current war. The last known war " \
+                f"in which this map occurred is unknown."
+        
+        super().__init__(message)
