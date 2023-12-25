7884cef8fae1deaaa1739c4fcbeeff5e07daa371
Louise Seale <addseale2+github@gmail.com>
2023-12-25 16:49:59 -0600

    Tucks execution under name=main

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ with your own bot account token and so on.
 import discord
 import config
 from logging import Logger
+from datetime import datetime
 
 # Set up logging
 log = Logger("PFL-TK Log")
@@ -23,22 +24,35 @@ own_name = config.discord_bot_account_name
 intents = discord.Intents.default()
 intents.message_content = True
 
-# Initialize the client using the specified intents
-client = discord.Client(intents=intents)
 
-# Set an event to announce in log when ready
-@client.event
-async def on_ready():
-    log.info(f'PFL-TK logged in as {client.user}')
+class PFLTK_DiscordClient(discord.Client):
+    async def on_ready(self):
+        message = f"PFL-TK Client for bot {own_name} launched"
+        log.info(message)
 
-# Set an event to handle incoming messages
-@client.event
-async def on_message(message: discord.Message):
-    if message.author != own_name:
-        response = \
-            f'Greetings {message.author}. Your message reads: {message.content}'
-        await message.channel.send(response)
+    async def on_message(self, message: discord.Message):
+        if message.author != own_name:
+            await message.channel.send(f"Message heard from author {message.author}. You said:\n{message.content}")
 
-# If this file was called directly as a script, host the bot.
-if __name__ == "__main__":
-    client.run(token=token)
+client = PFLTK_DiscordClient(intents=intents)
+client.run(token=token)
+
+# # Initialize the client using the specified intents
+# client = discord.Client(intents=intents)
+
+# # Set an event to announce in log when ready
+# @client.event
+# async def on_ready():
+#     log.info(f'PFL-TK logged in as {client.user}')
+
+# # Set an event to handle incoming messages
+# @client.event
+# async def on_message(message: discord.Message):
+#     if message.author != own_name:
+#         response = \
+#             f'Greetings {message.author}. Your message reads: {message.content}'
+#         await message.channel.send(response)
+
+# # If this file was called directly as a script, host the bot.
+# if __name__ == "__main__":
+#     client.run(token=token)
